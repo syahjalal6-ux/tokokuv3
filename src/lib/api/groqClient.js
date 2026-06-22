b9e867d089dd1047a643075300d922e010cd7e10
syahjalal6-ux <syahjalal6@gmail.com>
2026-06-22 11:54:19 +0700

    Add groqClient.js with chatApi and showcaseChatApi

---
+++ src/lib/api/groqClient.js
@@ -0,0 +1,13 @@
+import { CONFIG } from '../config.js'
+
+export const chatApi = {
+  send: async ({ messages, produk, toko, semuaProduk }) => {
+    // ... salin isi chatApi.send dari supabase.js lama
+  }
+}
+
+export const showcaseChatApi = {
+  send: async ({ messages, posts, produkList }) => {
+    // ... salin isi showcaseChatApi.send dari supabase.js lama
+  }
+}
